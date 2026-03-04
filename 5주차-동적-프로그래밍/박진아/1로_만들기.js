098be0719cb079bbfdd10d344c7a3583725b4a8b
gnath <rudqor1618@naver.com>
2026-03-04 09:44:59 +0900

    5주차 1일차 박진아: DP 문제풀이

---
+++ 5주차-동적-프로그래밍/박진아/1로_만들기.js
@@ -0,0 +1,16 @@
+function makeToOne(X) {
+  const dp = new Array(X + 1).fill(0);
+
+  for(let i = 2; i <= X; i++) {
+    dp[i] = dp[i - 1] + 1;
+
+    if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+    if (i % 5 === 0) dp[i] = Math.min(dp[i], dp[i / 5] + 1);
+
+  }
+
+  return dp[X];
+}
+
+console.log(makeToOne(26));
